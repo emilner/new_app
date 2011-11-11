bb5ecf79894f8529d5bca5957f214102c0bd1195
Emma Milner <emma@milner-henriksen.com>
2011-11-11 16:23:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlakeNew::Application.config.secret_token = 'ec5a47244abde759c7cb64629e9a5031637885fbb6cc0238851d68b0ca8b05ca33e349dea0138cdb5c933bf3c619962fa809ca48251f862c3905be4108303d77'
